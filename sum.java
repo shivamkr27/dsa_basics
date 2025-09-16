79a2c4f098d3fc474388bceef774143e0330bbfa
shivamkr27 <shivamkumarbxr8@gmail.com>
2025-09-16 19:34:22 +0530

    Q1: Write a program to input two numbers and display their sum.

---
+++ sum.java
@@ -0,0 +1,12 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+import java.util.*;
+class sum{
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int a = scn.nextInt();
+    int b =scn.nextInt();
+  
+    System.out.println("sum = " +(a+b));
+  }
+}
